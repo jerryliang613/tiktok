5d19034f3edcafb57266e5748446f33ebda6cbcb
Jerry <jerry.liang613@gmail.com>
2021-01-08 21:31:36 +1100

    loading icons added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,37 @@
 import Video from './components/Video';
 import http from './axios';
 import { useState, useEffect } from 'react';
+import CircularProgress from '@material-ui/core/CircularProgress';
 
 function App() {
-  const [videos, setVideos] = useState([]);
+  const [videos, setVideos] = useState({
+    data: [],
+    loading: true
+  });
+
   useEffect(() => {
     async function fetchPosts() {
       const response = await http.get();
-      setVideos(response.data);
+      setVideos({
+        data: response.data,
+        loading: false
+      });
       return response;
     };
-    fetchPosts();
+    try {
+      fetchPosts();
+    } catch {
+      setVideos({
+        ...videos, loading: false
+      })
+    }
 
-  }, [])
+  }, []);
+  if (videos.loading) return <div className='loading'><CircularProgress color='secondary' /> </div>
   return (
     <div className="app">
       <div className="app_video">
-        {videos
+        {videos.data
           .map((video, index) => <Video
             key={index}
             video={video.url}
